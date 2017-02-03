a4e44593ce563ed5ce9714b69cc21d4734f0474d
Cris Ryan Tan <crisryantan@gmail.com>
2017-02-03 21:01:54 +0800

    setup reducer for receiving posts
    - setup basic route
    - setup initial form

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
 
-export default () => {
-	return <div>List of blog posts.</div>
+class PostsIndex extends Component {
+
+	componentWillMount () {
+		this.props.fetchPosts();
+	}
+
+	render () {
+		return (
+			<div>
+				<div className="text-xs-right">
+					<Link to="/posts/new" className="btn btn-primary">
+						Add a post
+					</Link>
+				</div>
+				List of blog posts
+			</div>
+		);
+	}
 }
+
+export default connect( null, { fetchPosts } )( PostsIndex );
--- src/index.js
+++ src/index.js
@@ -6,7 +6,13 @@ import { Router, browserHistory } from 'react-router';
 import reducers from './reducers';
 import routes from './routes';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// makes sure that all of our actions flow through the promised middleware before reaching
+// the reducers
+import promise from 'redux-promise';
+
+const createStoreWithMiddleware = applyMiddleware(
+	promise
+)(createStore);
 
 ReactDOM.render(
 	<Provider store={createStoreWithMiddleware(reducers)}>
